1ab32b86c0eae63e55fa530b1900111ecf9c7742
Chris Paulusson <chrispaulusson@c-10-253-240-229.clients.eduroam.liu.se>
2019-02-22 12:55:24 +0100

    fixed bugs in domino.cs

--- Unity/domino.cs
+++ Unity/domino.cs
@@ -2,20 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class domino : MonoBehaviour {
 
 	public int force = 100;
 
-	public int m = 1;
+	public float m = 1f;
 	public float g = 9.82f;
-	public int H = 2;
-	public int B = 1;
-	private float r = sqrt(Math.pow(H, 2) + Math.pow(B, 2));
+	public static float H = 2f;
+	public static float B = 1f;
+	private float r = Mathf.Sqrt(Mathf.Pow(H, 2) + Mathf.Pow(B, 2));
 
-	public int tTot = 10;
-	public float h = 0.005f;
-	private int N = (int)(tTot/ h);
+	public static int tTot = 10;
+	public static float h = 0.005f;
+	private static int N = (int)(tTot/ h);
 
 	private float[] alpha = new float[N];
 	private float[] omega = new float[N];
@@ -28,22 +27,21 @@ public class domino : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		for(double t = 0; t < tTot; t += h;)
+		for(double t = 0; t < tTot; t += h)
 		{
-			a[n + 1] = ((1/(Math.pow(m,2)))*(force*r) - (g*m*Distance(theta, H, n)));
-			omega[n + 1] = omega[n] + a[n]*h;
+			alpha[n + 1] = ((1/(Mathf.Pow(m,2)))*(force*r) - (g*m*Distance(theta[n], H, n)));
+			omega[n + 1] = omega[n] + alpha[n]*h;
 			theta[n + 1] = theta[n] + omega[n]*h;
 
 			if (theta[n+1] >= (Mathf.PI/2))
 			{
 				theta[n+1] = (Mathf.PI/2);
-				a[n + 1] = 0f;
+				alpha[n + 1] = 0f;
 				omega[n + 1] = 0f;
 				break;
 			}
 			n++;
 		}
-
 	}
 
 	void Force(){
@@ -52,27 +50,30 @@ public class domino : MonoBehaviour {
 			force = 0;
 		}
 	}
-
-	float Distance(float theta, int H, int n)
+	float d;
+	float Distance(float theta, float H, int n)
 	{
+		
 		if(Time.deltaTime == 0)
 		{
-			return (float)H/2;
+			d = (float)H/2;
 		}
 		else
 		{
-			if(theta > (Math.PI/4))	
+			if(theta > (Mathf.PI/4))	
 			{
-				d = -((float)(H / 2) * Math.sin (theta));
+				d = -((float)(H / 2) * Mathf.Sin (theta));
+
 			}
 			else
 			{
-				if(theta > (Math.PI/4))	
+				if(theta > (Mathf.PI/4))	
 				{
-					d = ((float)(H/2) * Math.sin(theta));
+					d = ((float)(H/2) * Mathf.Sin(theta));
+				
 				}
-
 			}
 		}
 		return d;
 	}
+}
