ae23d7705c553a9bd0fbe4e248f8f0d6632b6771
pontus arnesson <pontusarnesson@c-10-253-192-229.clients.eduroam.liu.se>
2019-02-28 09:05:24 +0100

    Nu snurrar den iallafall

--- Unity/domino.cs
+++ Unity/domino.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class domino : MonoBehaviour
+public class NewBehaviourScriptTopple : MonoBehaviour
 {
 
-    public float force = 0.005f;
+    public float force = 100f;
 
     public float m = 0.00456f;
     public float g = 9.82f;
@@ -23,16 +23,20 @@ public class domino : MonoBehaviour
     private float[] theta = new float[N];
 
     private int n = 0;
+
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
+        print(" baaaaajs" + transform.eulerAngles.x);
     }
 
     // Update is called once per frame
     void Update()
     {
-       
-
+     
             alpha[n + 1] = ((1 / (Mathf.Pow(m, 2))) * (force * r) - (g * m * Distance(theta[n], H, n)));
             omega[n + 1] = omega[n] + alpha[n] * h;
             theta[n + 1] = theta[n] + omega[n] * h;
@@ -42,19 +46,30 @@ public class domino : MonoBehaviour
                 theta[n + 1] = (Mathf.PI / 2);
                 alpha[n + 1] = 0f;
                 omega[n + 1] = 0f;
+            rb.transform.Rotate(0, omega[n + 1], 0);
+        }
 
-            }
+
+        if (theta[n] < 1.58)
+        {
+            rb.transform.Rotate(0, omega[n + 1], 0);
+        }
+
+        print("The variable is :" + theta[n] + " on " + n + "samples");
+            
             n++;
-        
+        print("N is for fuck sake: " + n);
+
     }
 
     void Force()
     {
-        if (Time.deltaTime > 0.1)
+        if (Time.deltaTime > 0.5)
         {
             force = 0;
         }
     }
+
     float d;
     float Distance(float theta, float H, int n)
     {
@@ -81,5 +96,6 @@ public class domino : MonoBehaviour
         }
         return d;
     }
-        
+
+
 }
